f9a2daa01baaebd1f9809b201d67afc1fca6b83a
Amoolaa <86513920+Amoolaa@users.noreply.github.com>
2023-04-23 02:55:48 +1000

    testing

---
+++ app/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_session import Session
+from model_training import ModelTrainingRunner
+import pickle
+
+# 18481648 4860090466 337630337 65722733 712926286591762433 244168999 207581304 1333768694112129026 2427400058 697785916623036416 277622247 1192024076006699008 1107588313509253120
+
+app = Flask(__name__)
+sess = Session()
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+@app.route("/model_training/submit", methods=['POST', 'GET'])
+def submit():
+  if request.method == "POST":
+    ids = request.form["user_ids"].split(" ")
+    category = request.form["category"]
+    max_tweets = int(request.form["max_tweets"])
+    mode = request.form["test"]
+    if mode == "on":
+      mode = "stub"
+    else:
+      mode = "normal"
+    session["model_trainer"] = ModelTrainingRunner(ids=ids, category=category, max_tweets=max_tweets, mode=mode)
+    return redirect(url_for("update_tweet", id=session["model_trainer"].get_latest_tweet()["id"]))
+  else:
+    return render_template("submit.html")
+
+@app.route("/model_training/<id>",  methods=['POST', 'GET'])
+def update_tweet(id):
+  if session["model_trainer"].max_tweets == 0:
+    session["model_trainer"].tweets.to_csv("datasets/" + session["model_trainer"].category + "_labelled.csv")
+    pickle.dump(session["model_trainer"].model, open("models/" + session["model_trainer"].category + ".sav", 'wb'))
+    return redirect(url_for("classify"))
+  elif request.method == "POST":
+    session["model_trainer"].max_tweets -= 1
+    session["model_trainer"].update_tweets_and_model(request.form["submit_button"])
+    new_id = session["model_trainer"].get_latest_tweet()["id"]
+    return render_template("temp.html", id=new_id)
+  else:
+    return render_template("temp.html", id=id)
+
+@app.route("/classify_tweets", methods=['POST', 'GET'])
+def classify():
+  return "Hello World!"
+
+if __name__ == "__main__":
+  app.secret_key = 'super secret key'
+  app.config['SESSION_TYPE'] = 'filesystem'
+
+  sess.init_app(app)
+
+  app.debug = True
+  app.run()
---
+++ app/model_training.py
@@ -0,0 +1,186 @@
+import pickle
+import pandas as pd
+import numpy as np
+import argparse
+
+import data_collection_and_cleaning as dc
+
+import sklearn
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neural_network import MLPClassifier
+
+
+class ModelTrainingRunner:
+
+  def __init__(self, ids, category, max_tweets, mode):
+    self.mode = mode
+    self.max_tweets = max_tweets
+    self.ids = ids
+    self.category = category
+
+    self.tweets, self.model = init(category, ids, mode)
+    self.tweets = tweets_classification(self.model, self.tweets)
+
+  def get_latest_tweet(self):
+    unlabelled_tweets = self.tweets[self.tweets['label'].isnull()]
+    unlabelled_tweets_0 = unlabelled_tweets[unlabelled_tweets['pred'] == 0]
+    return unlabelled_tweets_0.iloc[0]
+
+  # Updates tweets once
+  def update_tweets_and_model(self, response):
+    self.tweets, self.model = push_tweet_and_get_input(self.model, self.tweets, response)
+
+
+
+  
+
+
+# The function to start the script, including the whole process of the workflow.
+def start(ids, category, max_num_tweets_to_view, mode):
+
+    # Initialize the dataset (tweets) and model
+    tweets, model = init(category, ids, mode)
+    tweets = tweets_classification(model, tweets)
+
+    # Workflow: push the tweet and receive feedback
+    for i in range(0, max_num_tweets_to_view):
+        tweets, model = push_tweet_and_get_input(model, tweets)
+
+    # Retrain the model again
+    model = retrain(model, tweets)
+    tweets = tweets_classification(model, tweets)
+    
+    # Save the labelled tweets and the trained model
+    tweets.to_csv("datasets/" + category + "_labelled.csv")
+    pickle.dump(model, open("models/" + category + ".sav", 'wb'))
+
+    print("End train....")
+
+# The function for program initialization
+def init(category, ids, mode='normal'):
+    print("Starting to collect tweets from the entered users... ...")
+
+    # Init according to the mode selected. 
+    # A normal mode is for normal data gathering and cleaning.
+    # The stub mode is for loading cleaned data for testing. (Avoid call Tweepy too often.)
+    if mode != 'normal':
+        df = collect_and_clean_tweets(category, ids)
+    else:
+        # stub
+        df = collect_and_clean_tweets_stub(category)
+    
+    # Init database and model
+    df = init_database(df)
+    model = init_model(df)
+
+    return df, model
+
+# Tweets collection and cleaning.
+def collect_and_clean_tweets(category, ids):
+    dc.collect_tweets(category, ids)
+    df = dc.clean_tweets(category)
+    return df
+
+# Tweets collection and cleaning. (Stub)
+def collect_and_clean_tweets_stub(category):
+    # direct load from datasets
+    df = pd.read_csv("datasets/" + category + ".csv")
+    return df
+
+# Init database (dataframe)
+def init_database(df):
+    df["pred"] = np.random.randint(0,2, size=len(df))
+    df["label"] = None
+    return df
+
+# Init model for text classification.
+def init_model(df):
+    # create an empty model
+    text_clf = Pipeline([
+     ('vect', CountVectorizer()),   # converts text into feature vectors (see bag-of-words)
+     ('tfidf', TfidfTransformer()), # converts to frequencies + applying weighting to certain tokens (see tf-idf)
+     ('clf', MLPClassifier(hidden_layer_sizes=(10), activation='relu', solver='lbfgs', random_state=42, max_iter=200)),      # model, we can replace this with whatever model we want
+    ])
+    
+    # Train with init dataset df
+    x = df["text"]
+    y = df["pred"]
+
+    text_clf.fit(x, y)
+
+    return text_clf
+
+# The function for tweets classification according to the current model
+def tweets_classification(model, tweets):
+    # classify tweets based on the current model
+    predicts = model.predict(tweets["text"])
+    tweets["pred"] = predicts
+
+    return tweets
+
+def get_tweet(model, tweets):
+    unlabelled_tweets = tweets[tweets['label'].isnull()]
+    return unlabelled_tweets[unlabelled_tweets['pred'] == 0]
+
+def push_tweet_and_get_input(model, tweets, response):
+    # push the first tweets unlabelled and predicted as 0
+    unlabelled_tweets = tweets[tweets['label'].isnull()]
+    unlabelled_tweets_0 = unlabelled_tweets[unlabelled_tweets['pred'] == 0]
+
+    push_tweet = unlabelled_tweets_0.iloc[0]
+    
+    if response == "Yes":
+        tweets = confirm(tweets, push_tweet)
+    elif response == "No":
+        tweets = wrong(tweets, push_tweet)
+    else:
+        model = retrain(model, tweets)
+        tweets = tweets_classification(model, tweets)
+    
+    return tweets, model
+
+# Option 1
+def confirm(tweets, push_tweet):
+    target_id = push_tweet['id']
+    tweets.loc[tweets['id'] == target_id,'label'] = 0
+    
+    return tweets
+
+# Option 2
+def wrong(tweets, push_tweet):
+    target_id = push_tweet['id']
+    tweets.loc[tweets['id'] == target_id,'label'] = 1
+
+    return tweets
+
+# Option 3
+def retrain(model, tweets):
+    print("Retrain the model... ...")
+    tweets_labelled = tweets[~tweets['label'].isnull()]
+    x = tweets_labelled["text"]
+    y = tweets_labelled["label"]
+    y=y.astype('int')
+
+    model.fit(x, y)
+    return model
+
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser()
+
+    # Add an argument
+    parser.add_argument("-m", "--mode", action='store_true')
+
+    # Parse the argument
+    args = parser.parse_args()
+
+    mode = None
+    if args.mode == True:
+        mode = 'stub'
+    else:
+        mode = 'normal'
+
+    # Start the script
+    start(mode)
